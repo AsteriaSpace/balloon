3337746a541c7411c6b170d0b72c5fa01b639e83
Helen <helen_kattaleeya@outlook.com>
2020-02-21 19:44:29 +0000

    Update WB_pressure.py

--- WB_pressure.py
+++ WB_pressure.py
@@ -1,66 +1,102 @@
-#useful libraries
+import smbus
 import time
-import csv
-import quick2wire.i2c as i2c
-from quick2wire.i2c import I2CMaster, writing_bytes, reading
-import math
 
-#code template from: https://github.com/rsolomon/py-MS5607
+#sample code from https://github.com/ControlEverythingCommunity/MS5803-02BA/blob/master/Python/MS5803_02BA.py
 
-#useful registers
-#PLEASE CHECK
-RESET = 0x1E
-ADC_READ = 0x00
-ADC_CONV = 0x40
-ADC_D1 = 0x00
-ADC_D2 = 0x10
-ADC_256 = 0x00
-ADC_512 = 0x02
-ADC_1024 = 0x04
-ADC_2048 = 0x06
-ADC_4096 = 0x08
-PROM_RD = 0xA0
+# Get I2C bus
+bus = smbus.SMBus(1)
 
 
-def __read_adc(device_addr, cmd):
-		with I2CMaster() as master:
-			master.transaction(
-				writing_bytes(device_addr, (DC_CONV | cmd)))  # Send conversion command
-
-		# Map of times to delay for conversions
-		delay_time = {}
-		delay_time[_CMD_ADC_256] = 0.001
-		delay_time[_CMD_ADC_512] = 0.003
-		delay_time[_CMD_ADC_1024] = 0.004
-		delay_time[_CMD_ADC_2048] = 0.006
-		delay_time[_CMD_ADC_4096] = 0.01
-
-		time.sleep(delay_time[cmd & 0x0f]) # Wait necessary conversion time
-
-		with I2CMaster() as master:
-			read_bytes = master.transaction(
-				writing_bytes(device_addr, ADC_READ),
-				reading(address, 3))
-
-		tmp = 65536 * read_bytes[0][0] # Read MSB
-		tmp = tmp + 256 * read_bytes[0][1] # Read byte
-		tmp = tmp + read_bytes[0][2] # Read LSB
-		return tmp
+#define useful registers
+DEVICE_ADDR_1 = 0x76
+RESET = 0.1E
 
 
-def get_pressure():
-    pressure = self.__read_adc(_CMD_ADC_D1 | _CMD_ADC_4096)
 
-#defining start time
-t0 = time.time()
 
-with  open('pres_Vals.csv', mode='w') as pres_data:
-    pres_writer = csv.writer(pres_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    t0 = time.time()
-    # keep reading data when elapse time is less than 2 hours
-    while (t0 - time.time()) < 7200:
-        #returns temperature in celsius
-        pres = adt.temperature()
-        pres_writer.writerow([temp, time.time())
-        #delay to allow for data to be collected
-        sleep(1)
+
+# MS5803_02BA address, 0x76(118)
+#		0x1E(30)	Reset command
+bus.write_byte(0x76, 0x1E)
+
+time.sleep(0.5)
+
+# Read 12 bytes of calibration data
+# Read pressure sensitivity
+data = bus.read_i2c_block_data(0x76, 0xA2, 2)  #0xA2 =
+C1 = data[0] * 256 + data[1]
+
+# Read pressure offset
+data = bus.read_i2c_block_data(0x76, 0xA4, 2)
+C2 = data[0] * 256 + data[1]
+
+# Read temperature coefficient of pressure sensitivity
+data = bus.read_i2c_block_data(0x76, 0xA6, 2)
+C3 = data[0] * 256 + data[1]
+
+# Read temperature coefficient of pressure offset
+data = bus.read_i2c_block_data(0x76, 0xA8, 2)
+C4 = data[0] * 256 + data[1]
+
+# Read reference temperature
+data = bus.read_i2c_block_data(0x76, 0xAA, 2)
+C5 = data[0] * 256 + data[1]
+
+# Read temperature coefficient of the temperature
+data = bus.read_i2c_block_data(0x76, 0xAC, 2)
+C6 = data[0] * 256 + data[1]
+
+# MS5803_02BA address, 0x76(118)
+#		0x40(64)	Pressure conversion(OSR = 256) command
+bus.write_byte(0x76, 0x40)
+
+time.sleep(0.5)
+
+# Read digital pressure value
+# Read data back from 0x00(0), 3 bytes
+# D1 MSB2, D1 MSB1, D1 LSB
+value = bus.read_i2c_block_data(0x76, 0x00, 3)
+D1 = value[0] * 65536 + value[1] * 256 + value[2]
+
+# MS5803_02BA address, 0x76(118)
+#		0x50(64)	Temperature conversion(OSR = 256) command
+bus.write_byte(0x76, 0x50)
+
+time.sleep(0.5)
+
+# Read digital temperature value
+# Read data back from 0x00(0), 3 bytes
+# D2 MSB2, D2 MSB1, D2 LSB
+value = bus.read_i2c_block_data(0x76, 0x00, 3)
+D2 = value[0] * 65536 + value[1] * 256 + value[2]
+
+dT = D2 - C5 * 256
+TEMP = 2000 + dT * C6 / 8388608
+OFF = C2 * 131072 + (C4 * dT) / 64
+SENS = C1 * 65536 + (C3 * dT ) / 128
+T2 = 0
+OFF2 = 0
+SENS2 = 0
+
+if TEMP >= 2000 :
+	T2 = 0
+	OFF2 = 0
+	SENS2 = 0
+elif TEMP < 2000 :
+	T2 = (dT * dT) / 2147483648
+	OFF2= 61 * ((TEMP - 2000) * (TEMP - 2000)) / 16
+	SENS2= 2 * ((TEMP - 2000) * (TEMP - 2000))
+	if TEMP < -1500 :
+		OFF2 = OFF2 + 20 * ((TEMP + 1500) * (TEMP + 1500))
+		SENS2 = SENS2 + 12 * ((TEMP + 1500) * (TEMP +1500))
+
+TEMP = TEMP - T2
+OFF = OFF - OFF2
+SENS = SENS - SENS2
+pressure = ((((D1 * SENS) / 2097152) - OFF) / 32768.0) / 100.0
+cTemp = TEMP / 100.0
+
+
+# Output data to screen
+print "Pressure : %.2f mbar" %pressure
+print "Temperature in Celsius : %.2f C" %cTemp
